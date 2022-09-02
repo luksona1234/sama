791c30731cf351ce7657246d228907138c4c2e39
luksona1234 <lukakhachvani0311@gmail.com>
2022-09-02 09:56:16 +0400

    js

---
+++ main.js
@@ -0,0 +1,99 @@
+let empty = [];
+for (let i = 0; i <= 9; i++){
+    if(i % 2 == 0){
+        empty.push(i);
+        console.log(empty);
+    }
+}
+
+
+// for (let i = 0; i < myArr.length; i++){
+    
+// }
+// let sum = 0
+// let myArr = [2, 3, 4, 5, 6]
+// let sim = sum + (myArr.indexOf(2))
+// console.log(sim);
+
+// n = 2
+
+
+// while(n<=13){
+//     n++
+//     console.log(n);
+//     n = (n - 1) + (n - 2)
+//     console.log(n);
+// }
+
+
+l = 0
+
+while(l<20){
+    l++
+    if(l % 3 ==0){
+        console.log(l);
+    }
+
+}
+
+let array4 = [142, 154, 366]
+for(i of array4){
+    console.log(i);
+}
+for(s of array4){
+    console.log(s / 2);
+}
+
+
+let empty1 = [];
+for (let i = 0; i <= 9; i++){
+    if(i % 2 == 1){
+        empty1.push(i);
+        console.log(i)
+    }
+}
+
+var numbers = [1, 2, 44, 64, 55, 24, 32, 55, 19, 17, 22, 23];
+var evenNumbers =[]
+var oddNumbers = []
+
+for(let c = 0; c < 23; c++){
+    if(c % 2 == 0){
+        evenNumbers.push(c)
+        console.log(evenNumbers);
+    }
+    if(c % 2 == 1){
+        oddNumbers.push(c)
+        console.log(oddNumbers);
+    }
+}
+
+let luka = []
+let luka1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+for(let i = 0; i <= 20; i++){
+    if(i % 4 == 0){
+        luka.push(i);
+        console.log(luka);
+    }
+}
+
+
+for(let j = 0; j <= 50; j++){
+    if(j % 2 == 1){
+        console.log(j);
+    }
+}
+let str = 'numbers less than 15'
+for(let u = 0; u <= 30; u++){
+    if(u > 15){
+        console.log(u);
+    }else if(u < 15){
+        console.log(str);
+    }
+    
+}
+
+
+
+
